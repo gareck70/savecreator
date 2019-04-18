a7f8c70f432ae83c938b0116fc43ae4f12a04cfd
cx61grit <cx61grit@cx61grit-PC>
2019-04-18 16:31:27 +0900

    asdasd

---
+++ src/main/java/com/savecreator/web/controller/NoticeController.java
@@ -0,0 +1,17 @@
+package com.savecreator.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/notice/")
+public class NoticeController {
+	
+	@RequestMapping("notice-list")
+	public String noticelist() {
+		
+		return "notice.notice-list";
+	}
+
+	
+}
